0b741e926e65a8223d6d0b21ab8463646369d1d0
Vladislav Vasilevskiy <91417982+Karmorant@users.noreply.github.com>
2023-05-18 12:18:31 +0300

    Add files via upload

--- Graph_edit.py
+++ Graph_edit.py
@@ -1,6 +1,7 @@
 import numpy
 import matplotlib.pyplot as plt
 from numpy import loadtxt
+import math
 
 
 with open("settings.txt", 'r') as f:
@@ -21,9 +22,9 @@ with open("settings.txt", 'r') as f:
         "Процесс зарядки и разрядки конденцатора в RC-цепочке", loc='center', fontsize=20)
 
     plt.xlabel('Время, с', fontsize=15)
-    plt.xlim(0, 100)
+    plt.xlim(0, expTime)
     plt.ylabel('Напряжение, B', fontsize=15)
-    plt.ylim(0, 3.5)
+    plt.ylim(0, math.ceil(max(loadtxt("data.txt") * discr)))
 
     plt.plot(numpy.linspace(0, expTime, int(expTime / quant)),
              [i * discr for i in loadtxt("data.txt")], 'o',  linestyle="solid", markevery=25, color='b', label="V(t)")
@@ -33,7 +34,7 @@ with open("settings.txt", 'r') as f:
     plt.annotate("Время зарядки: {}, s".format(round(numpy.argmax(
         loadtxt("data.txt")) * quant)), (70, 2), fontsize=10)
 
-    plt.annotate("Время зрзрядки: {}, s".format(round(
+    plt.annotate("Время разрядки: {}, s".format(round(
         expTime - numpy.argmax(loadtxt("data.txt")) * quant)), (70, 2.5), fontsize=10)
 
     plt.savefig("Capasitor_Graph")
